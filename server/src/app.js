9a886394f3b31b75d814233036d0db5f63f37bbb
riteshNikam <riteshnikam2804@gmail.com>
2025-01-10 09:14:58 +0530

    cors test 1

--- server/src/app.js
+++ server/src/app.js
@@ -1,36 +1,38 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import { Server } from "socket.io"
-import { createServer } from "node:http"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
-const app = express() 
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({limit: "16kb", extended: true}))
-app.use(cookieParser())
-app.use(cors({
-    origin: '*',
-    credentials: true
-}))
+app.use(express.json());
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://chat-app-i9kb-git-main-riteshnikams-projects.vercel.app/",
+    credentials: true,
+  })
+);
 
-import { userRouter } from "./routes/user.routes.js"
-import { messageRouter } from "./routes/message.routes.js"
+import { userRouter } from "./routes/user.routes.js";
+import { messageRouter } from "./routes/message.routes.js";
 
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/messages', messageRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/messages", messageRouter);
 
-const server = createServer(app)
+const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: '*'
-    }
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-io.on('connection', (socket) => {
-    socket.on('message', message => {
-        io.emit('message', message) // io.emit will broadcast the event to all the connected sockets
-    })
-})
+io.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    io.emit("message", message); // io.emit will broadcast the event to all the connected sockets
+  });
+});
 
-export { server }
+export { server };
